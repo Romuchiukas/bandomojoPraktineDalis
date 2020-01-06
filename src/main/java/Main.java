13b0e6395def2421159972cbceb9dc43cf55aa68
Romuchiukas <romuciukas@yahoo.com>
2020-01-06 11:53:52 +0200

    Dienynas su Hibernate

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        spausdintiStudentoInf();
+//        spausdintiStudentoInf();
         uzkrautiDuomenis();
         kursoVidurkis(2018);
     }
